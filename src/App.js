9d2a30bc7897db32c02b4c303214c6dfa23e8c42
alexVezer <alexartimon@gmail.com>
2020-01-09 19:31:21 +0200

    now loading from external file

--- src/App.js
+++ src/App.js
@@ -2,52 +2,29 @@ import React, {Component} from 'react';
 import Tachyons from 'tachyons';
 import Header from './components/Header.js';
 import Poza from './components/Poza.js';
+import poze from './poze.js';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      imgs:[
-        {
-     id:1,     
-    link:'https://static.pexels.com/photos/52500/horse-herd-fog-nature-52500.jpeg',
-    desc:'Horse'
-  },
-  {
-    id:2,
-    link:'https://static.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg',
-    desc:'Elephant cub'
-  },
-  {
-    id:3,
-    link:'https://static.pexels.com/photos/213399/pexels-photo-213399.jpeg',
-    desc:'Fishes'
+    this.state = { poze }
   }
-      ]
-
-    }
-  }
-
-// componentDidMount() {
-//   fetch('./poze.js')
-//     .then(response=> response.json())
-//     .then(poze => this.setState({imgs:poze}))
-// }
+  
 
 render() {
-  console.log(this.state.imgs);
+  console.log(this.state.poze);
   return (
 
     <div className="tc">
       <Header title="Gallery"/>
 
       <div className="container" > {
-        this.state.imgs.map((poz) => (
+        this.state.poze.map((poz) => (
             <Poza link={poz.link} desc={poz.desc} /> 
                     ))
       }
-      }
+      
       </div>
     </div>
   );
--- src/poze.js
+++ src/poze.js
@@ -10,7 +10,20 @@ export const poze = [
 	{
 		link:'https://static.pexels.com/photos/213399/pexels-photo-213399.jpeg',
 		desc:'Fishes'
+	},
+	{
+		link:'https://static.pexels.com/photos/158471/ibis-bird-red-animals-158471.jpeg',
+		desc:'Ibis Bird'
+	},
+	{
+		link:'https://static.pexels.com/photos/133459/pexels-photo-133459.jpeg',
+		desc:'Lemuri'
+	},
+	{
+			link:'https://static.pexels.com/photos/50988/ape-berber-monkeys-mammal-affchen-50988.jpeg',
+			desc:'Monkey'
 	}
+
 ];
 
 export default poze;
